3a9bdf4940e864b4e2b0e3af788d05266c4d92f4
bamba-cr <67812357+bamba-cr@users.noreply.github.com>
2025-03-03 14:16:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,8 @@ import EditarAluno from '@/pages/EditarAluno';
 import Frequencia from '@/pages/Frequencia';
 import ConsultaFrequencia from '@/pages/ConsultaFrequencia';
 import Notas from '@/pages/Notas';
-import Historico from '@/pages/Historico';
 import Relatorios from '@/pages/Relatorios';
 import StudentPerformance from '@/pages/StudentPerformance';
-import Configuracoes from '@/pages/Configuracoes';
 import NotFound from '@/pages/NotFound';
 
 function App() {
